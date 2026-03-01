b45b87ea1c5e9a8d25dbe71b61dffe0e9ab9c9f6
Cristian Perez <cperezlecaros@gmail.com>
2026-02-28 21:46:34 -0300

    Implementa navegacion

--- app/src/main/java/com/example/biodex/MainActivity.kt
+++ app/src/main/java/com/example/biodex/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.biodex
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.biodex.core.di.IoDispatcher
 import com.example.biodex.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,7 +18,7 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    @Inject @IoDispatcher lateinit var ioDispatcher: CoroutineDispatcher
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +26,36 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        binding.bottomNav.setupWithNavController(navController)
+
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.createSightingFragment -> {
+                    binding.bottomNav.visibility = View.GONE
+                    supportActionBar?.show()
+                }
+                R.id.homeFragment -> {
+                    binding.bottomNav.visibility = View.VISIBLE
+                    supportActionBar?.hide()
+                }
+                else -> {
+                    binding.bottomNav.visibility = View.VISIBLE
+                    supportActionBar?.show()
+                }
+            }
+        }
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
